6d32e41e4f14db66cc7bfd2d1ad89b5f8bc0057c
Niwesh Sah <sahniwesh@gmail.com>
2024-11-18 16:04:48 +0530

    updated

--- lib/screens/event_details_page.dart
+++ lib/screens/event_details_page.dart
@@ -1251,7 +1251,7 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
 
   Widget _buildImageCarousel() {
     return SizedBox(
-      height: 200.0,
+      height: 400.0,
       child: eventImages == null
           ? Center(
               child: Column(
